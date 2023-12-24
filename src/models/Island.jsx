84be80bab2b0c693151d13dc3dd0b8a1fb433f13
StigAkl <stigakl@outlook.com>
2023-12-24 18:46:17 +0100

    add some orbit controls

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -20,147 +20,147 @@ import { useFrame, useThree } from "@react-three/fiber";
 import islandScene from "../assets/3d/island.glb";
 
 export function Island({
-    isRotating,
-    setIsRotating,
-    setCurrentStage,
-    currentFocusPoint,
-    ...props
+  isRotating,
+  setIsRotating,
+  setCurrentStage,
+  currentFocusPoint,
+  ...props
 }) {
-    const islandRef = useRef();
-    const { nodes, materials } = useGLTF(islandScene);
+  const islandRef = useRef();
+  const { nodes, materials } = useGLTF(islandScene);
 
-    const { gl, viewport } = useThree();
+  const { gl, viewport } = useThree();
 
-    const lastX = useRef(0);
-    const rotationSpeed = useRef(0);
-    const dampingFactor = 0.95;
+  const lastX = useRef(0);
+  const rotationSpeed = useRef(0);
+  const dampingFactor = 0.95;
 
-    const eventDefaults = (e) => {
-        e.stopPropagation();
-        e.preventDefault();
+  const eventDefaults = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+  };
+  const handlePointerDown = (e) => {
+    eventDefaults(e);
+    setIsRotating(true);
+
+    const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+
+    lastX.current = clientX;
+  };
+
+  const handlePointerUp = (e) => {
+    eventDefaults(e);
+    setIsRotating(false);
+  };
+
+  const handlePointerMove = (e) => {
+    eventDefaults(e);
+    if (isRotating) {
+      const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+
+      const delta = (clientX - lastX.current) / viewport.width;
+
+      islandRef.current.rotation.y += delta * 0.01 * Math.PI;
+      lastX.current = clientX;
+      rotationSpeed.current = delta * 0.01 * Math.PI;
     }
-    const handlePointerDown = (e) => {
-        eventDefaults(e);
-        setIsRotating(true);
+  };
 
-        const clientX = e.touches ? e.touches[0].clientX : e.clientX;
-
-        lastX.current = clientX;
+  const handleKeyDown = (e) => {
+    if (e.key == "ArrowLeft") {
+      if (!isRotating) setIsRotating(true);
+      islandRef.current.rotation.y += 0.01 * Math.PI;
+    } else if (e.key === "ArrowRight") {
+      if (!isRotating) setIsRotating(true);
+      islandRef.current.rotation.y -= 0.01 * Math.PI;
     }
+  };
 
-    const handlePointerUp = (e) => {
-        eventDefaults(e);
-        setIsRotating(false);
+  const handleKeyUp = (e) => {
+    if (e.key === "ArrowLeft" || e.key === "ArrowRight") setIsRotating(false);
+  };
+
+  useEffect(() => {
+    const canvas = gl.domElement;
+    document.addEventListener("pointerdown", handlePointerDown);
+    document.addEventListener("pointerup", handlePointerUp);
+    document.addEventListener("pointermove", handlePointerMove);
+    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      document.removeEventListener("pointerdown", handlePointerDown);
+      document.removeEventListener("pointerup", handlePointerUp);
+      document.removeEventListener("pointermove", handlePointerMove);
+      document.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener("keyup", handleKeyUp);
+    };
+  }, [gl, handlePointerDown, handlePointerUp, handlePointerMove]);
+
+  useFrame(() => {
+    if (!isRotating) {
+      rotationSpeed.current *= dampingFactor;
+
+      if (Math.abs(rotationSpeed.current) < 0.001) {
+        rotationSpeed.current = 0;
+      }
+
+      islandRef.current.rotation.y += rotationSpeed.current;
+    } else {
+      const rotation = islandRef.current.rotation.y;
+
+      const normalizedRotation =
+        ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI);
+
+      switch (true) {
+        case normalizedRotation >= 5.45 && normalizedRotation <= 5.85:
+          //setCurrentStage(4);
+          break;
+        case normalizedRotation >= 0.85 && normalizedRotation <= 1.3:
+          //setCurrentStage(3);
+          break;
+        case normalizedRotation >= 2.4 && normalizedRotation <= 2.6:
+          //setCurrentStage(2);
+          break;
+        case normalizedRotation >= 4.25 && normalizedRotation <= 4.75:
+          //setCurrentStage(1);
+          break;
+        default:
+        //setCurrentStage(null);
+      }
     }
+  });
 
-    const handlePointerMove = (e) => {
-        eventDefaults(e);
-        if(isRotating) {
-            const clientX = e.touches ? e.touches[0].clientX : e.clientX;
-
-            const delta = (clientX - lastX.current) / viewport.width;
-
-            islandRef.current.rotation.y += delta * 0.01 * Math.PI;
-            lastX.current = clientX; 
-            rotationSpeed.current = delta * 0.01 * Math.PI;
-        };
-    }
-
-    const handleKeyDown = (e) => {
-        if(e.key == 'ArrowLeft') {
-            if(!isRotating) setIsRotating(true);
-            islandRef.current.rotation.y += 0.01 * Math.PI;
-        } else if(e.key === 'ArrowRight') {
-            if(!isRotating) setIsRotating(true);
-            islandRef.current.rotation.y -= 0.01 * Math.PI;
-        }
-    }
-
-    const handleKeyUp = (e) => {
-        if(e.key === 'ArrowLeft' || e.key === 'ArrowRight') setIsRotating(false);
-    }
-    
-    useEffect(() => {
-        const canvas = gl.domElement;
-        document.addEventListener('pointerdown', handlePointerDown);
-        document.addEventListener('pointerup', handlePointerUp);
-        document.addEventListener('pointermove', handlePointerMove);
-        document.addEventListener('keydown', handleKeyDown);
-        document.addEventListener('keyup', handleKeyUp);
-
-        return () => {
-            document.removeEventListener('pointerdown', handlePointerDown);
-            document.removeEventListener('pointerup', handlePointerUp);
-            document.removeEventListener('pointermove', handlePointerMove);
-            document.removeEventListener('keydown', handleKeyDown);
-            document.removeEventListener('keyup', handleKeyUp);
-        }
-    }, [gl, handlePointerDown, handlePointerUp, handlePointerMove]);
-
-    useFrame(() => {
-        if(!isRotating) {
-            rotationSpeed.current *= dampingFactor;
-
-            if(Math.abs(rotationSpeed.current) < 0.001) {
-                rotationSpeed.current = 0;
-            }
-
-            islandRef.current.rotation.y += rotationSpeed.current;
-        } else {
-            const rotation = islandRef.current.rotation.y;
-
-            const normalizedRotation =
-            ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI);
-    
-            switch (true) {
-                case normalizedRotation >= 5.45 && normalizedRotation <= 5.85:
-                //setCurrentStage(4);
-                break;
-                case normalizedRotation >= 0.85 && normalizedRotation <= 1.3:
-                //setCurrentStage(3);
-                break;
-                case normalizedRotation >= 2.4 && normalizedRotation <= 2.6:
-                //setCurrentStage(2);
-                break;
-                case normalizedRotation >= 4.25 && normalizedRotation <= 4.75:
-                //setCurrentStage(1);
-                break;
-                default:
-                //setCurrentStage(null);
-            }
-        }
-    });
-
-    return (
-        <a.group ref={islandRef} {...props}>
-            <mesh
-                geometry={nodes.polySurface944_tree_body_0.geometry}
-                material={materials.PaletteMaterial001}
-            />
-            <mesh
-                geometry={nodes.polySurface945_tree1_0.geometry}
-                material={materials.PaletteMaterial001}
-            />
-            <mesh
-                geometry={nodes.polySurface946_tree2_0.geometry}
-                material={materials.PaletteMaterial001}
-            />
-            <mesh
-                geometry={nodes.polySurface947_tree1_0.geometry}
-                material={materials.PaletteMaterial001}
-            />
-            <mesh
-                geometry={nodes.polySurface948_tree_body_0.geometry}
-                material={materials.PaletteMaterial001}
-            />
-            <mesh
-                geometry={nodes.polySurface949_tree_body_0.geometry}
-                material={materials.PaletteMaterial001}
-            />
-            <mesh
-                geometry={nodes.pCube11_rocks1_0.geometry}
-                material={materials.PaletteMaterial001}
-            />
-        </a.group>
-    );
+  return (
+    <a.group ref={islandRef} {...props}>
+      <mesh
+        geometry={nodes.polySurface944_tree_body_0.geometry}
+        material={materials.PaletteMaterial001}
+      />
+      <mesh
+        geometry={nodes.polySurface945_tree1_0.geometry}
+        material={materials.PaletteMaterial001}
+      />
+      <mesh
+        geometry={nodes.polySurface946_tree2_0.geometry}
+        material={materials.PaletteMaterial001}
+      />
+      <mesh
+        geometry={nodes.polySurface947_tree1_0.geometry}
+        material={materials.PaletteMaterial001}
+      />
+      <mesh
+        geometry={nodes.polySurface948_tree_body_0.geometry}
+        material={materials.PaletteMaterial001}
+      />
+      <mesh
+        geometry={nodes.polySurface949_tree_body_0.geometry}
+        material={materials.PaletteMaterial001}
+      />
+      <mesh
+        geometry={nodes.pCube11_rocks1_0.geometry}
+        material={materials.PaletteMaterial001}
+      />
+    </a.group>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,77 +1,83 @@
-import { Canvas } from '@react-three/fiber'
-import React, { Suspense, useState } from 'react'
-import Loader from '../components/Loader'
-import { Island } from '../models'
-import Sky from '../models/Sky'
-import Bird from '../models/Bird'
-import Plane from '../models/Plane'
+import { Canvas } from "@react-three/fiber";
+import React, { Suspense, useState } from "react";
+import Loader from "../components/Loader";
+import { Island } from "../models";
+import Sky from "../models/Sky";
+import Bird from "../models/Bird";
+import Plane from "../models/Plane";
+import { OrbitControls } from "@react-three/drei";
 
 const Home = () => {
-    const [isRotating, setIsRotating] = useState(false);
+  const [isRotating, setIsRotating] = useState(false);
 
-    const adjustIslandForScreenSize = () => {
-        let islandScale = null;
-        let islandPosition = [0, -6.5, -143];
-        let islandRotation = [0.1, 4.7, 0];
-        if (window.innerWidth < 768) {
-            islandScale = [1.5,1.5, 1.5];
-        } else {
-            islandScale = [3, 3, 3];
-        }
-
-        return [islandScale, islandPosition, islandRotation];
+  const adjustIslandForScreenSize = () => {
+    let islandScale = null;
+    let islandPosition = [0, -6.5, -143];
+    let islandRotation = [0.1, 4.7, 0];
+    if (window.innerWidth < 768) {
+      islandScale = [1.5, 1.5, 1.5];
+    } else {
+      islandScale = [3, 3, 3];
     }
 
-    const adjustPlaneForScreen = () => {
-        let screenScale, screenPosition; 
+    return [islandScale, islandPosition, islandRotation];
+  };
 
-        if(window.innerWidth < 768) {
-            screenScale = [1.5, 1.5, 1.5]; 
-            screenPosition = [0, -1.5, 0];
-        } else {
-            screenScale = [3,3,3];
-            screenPosition = [0, -1, -4];
-        }
-        return [screenScale, screenPosition];
+  const adjustPlaneForScreen = () => {
+    let screenScale, screenPosition;
+
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5];
+      screenPosition = [0, -1.5, 0];
+    } else {
+      screenScale = [3, 3, 3];
+      screenPosition = [0, -1, -4];
     }
+    return [screenScale, screenPosition];
+  };
 
-    const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
-    const [planeScale, planePosition] = adjustPlaneForScreen();
+  const [islandScale, islandPosition, islandRotation] =
+    adjustIslandForScreenSize();
+  const [planeScale, planePosition] = adjustPlaneForScreen();
 
-    return (
-        <section className='w-full h-screen relative'>
-            <Canvas
-                className={
-                    `w-full h-screen bg-transparent ${isRotating ? 
-                        'cursor-grabbing' : 'cursor-grab'}`}
-                camera={{ near: 0.1, far: 1000 }}
-            >
-                <Suspense fallback={<Loader />}>
-                    <directionalLight 
-                    intensity={1.5} 
-                    position={[1,1,1]}
-                    />
-                    <ambientLight intensity={0.5}/>
-                 <hemisphereLight 
-                    skyColor={"#b2e1ff"} 
-                    groundColor={"#000000"} 
-                    intensity={1} />
+  return (
+    <section className="w-full h-screen relative">
+      <Canvas
+        className={`w-full h-screen bg-transparent ${
+          isRotating ? "cursor-grabbing" : "cursor-grab"
+        }`}
+        camera={{ near: 0.1, far: 100000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight intensity={1.5} position={[1, 1, 1]} />
+          <OrbitControls zoomSpeed={1} target={islandPosition} />
+          <ambientLight intensity={0.5} />
+          <hemisphereLight
+            skyColor={"#b2e1ff"}
+            groundColor={"#000000"}
+            intensity={1}
+          />
 
-<Bird />
-                    <Island
-                        position={islandPosition}
-                        scale={islandScale}
-                        rotation={islandRotation}
-                        isRotating={isRotating}
-                        setIsRotating={setIsRotating}
-                        />
-                    <Sky isRotating={isRotating} />
+          <Bird />
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
+          />
+          <Sky isRotating={isRotating} />
 
-                    <Plane isRotating={isRotating} rotation={[0, 20, 0]} scale={planeScale} position={planePosition} />
-                </Suspense>
-            </Canvas>
-        </section>
-    )
-}
+          <Plane
+            isRotating={isRotating}
+            rotation={[0, 20, 0]}
+            scale={planeScale}
+            position={planePosition}
+          />
+        </Suspense>
+      </Canvas>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
